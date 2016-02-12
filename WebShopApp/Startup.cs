c1adc386dcd66a592dff3cf963418b80ab5d14f1
Lingzhe Liu <lingzhe.liu@outlook.com>
2016-02-12 22:34:41 +0100

    initial add

---
+++ WebShopApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebShopApp.Startup))]
+namespace WebShopApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
